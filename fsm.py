4c07cf2d2af4a09773af7da115d82cce21cb50c5
余炘宏 <36984238+anuuanuu@users.noreply.github.com>
2019-12-06 15:44:47 +0800

    Update fsm.py

--- fsm.py
+++ fsm.py
@@ -6,31 +6,37 @@ from utils import send_text_message
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
-
-    def is_going_to_state1(self, event):
+#1
+    def is_going_to_advanced(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
-
+        return text.lower() == "advanced"
+#2
     def is_going_to_state2(self, event):
         text = event.message.text
         return text.lower() == "go to state2"
-
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+#3
+    def is_going_to_showfunction(self,event):
+        text = event.message.text
+        return text.lower() == "help"
+###
+    def on_enter_advanced(self, event):
+        print("I'm entering advanced")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state")
+        send_text_message(reply_token, "enter '12'
+                          '13'")
        
         self.go_back()
 
-    def on_exit_state1(self):
-        print("Leaving state1")
+    def on_exit_advanced(self):
+        print("Leaving advanced")
 
-    def on_enter_state2(self, event):
-        print("I'm entering state2")
+    def on_enter_showfunction(self, event):
+        print("I'm entering showfunction")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
+        send_text_message(reply_token, "'advanced':
+'help'")
         self.go_back()
 
     def on_exit_state2(self):
